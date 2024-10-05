099c8e4cbc6d3e889b57eaa98612f31cfcc97679
Banghua Zhao <banghua.zhao@gmail.com>
2024-10-05 17:23:26 +1300

    Create github workflow (#4)
    * Create github workflow
    
    * Update workflow trigger branch
    
    * Update workflows folder name
    
    * Resolve action error
    
    * Update branch
    
    * Delete workflow folder
    
    * Add secrets environment variable to workflows yml
    
    * Reset build settings
    
    * Reset build settings
    
    * Update env
    
    * Debug
    
    * debug
    
    * fix secret is empty
    
    * fix environment
    
    * Remove debug echo
    
    * update simulator device name
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * Test
    
    * Set up local env
    
    * fix

--- AdRevenueWatch/AdRevenueWatchApp.swift
+++ AdRevenueWatch/AdRevenueWatchApp.swift
@@ -8,6 +8,15 @@ import SwiftUI
 
 @main
 struct AdRevenueWatchApp: App {
+    init() {
+        // In CI, this is nil as the scheme doesn't have this environment variable
+        // This environment is variable only in local scheme
+        if let GIDClientID = ProcessInfo.processInfo.environment["GIDCLIENTID"] {
+            let config = GIDConfiguration(clientID: GIDClientID)
+            GIDSignIn.sharedInstance.configuration = config
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
             AppView(
